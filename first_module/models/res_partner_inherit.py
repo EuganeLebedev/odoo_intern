8fb2b6657a9b9dab35f47767f50b3dde0859aabe
EuganeLebedev <adm1n.lebedev@ya.ru>
2022-04-19 09:46:28 +0300

    Задача 4 Добавлен вывод чекбокса и отметкой главного контакта в kanban view При простановке главного контакта у существующего снимается отметка is_primary

--- first_module/models/res_partner_inherit.py
+++ first_module/models/res_partner_inherit.py
@@ -1,10 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-#TODO
-# Добавить возможность изменить главный контакт
-# Вывод атрибута главного контакта в kanban view
-
 class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
     is_primary = fields.Boolean(string='Primary', default=False)
@@ -18,16 +14,14 @@ class ResPartnerInherit(models.Model):
     def _on_change_is_primary(self):
         if self.is_primary:
             if self.primary_contact_count > 1:
-                raise ValidationError('Главный контакт может быть только один')
-                # print(self.env.context.get('parent_id'))
-                # contacts = self.env['res.partner'].search([('parent_id', '=', self.parent_id.id)])
-                # for rec in contacts:
-                #     print(rec.name)
-                #     rec.is_primary = False
-                # for rec in self.parent_id.child_ids:
-                #     if rec.is_primary:
-                #         print(rec.id)
-                #         rec.is_primary = False
+                partner_id = self.parent_id._origin.id
+                # raise ValidationError('Главный контакт может быть только один')
+
+                contacts = self.env['res.partner'].search([('parent_id', '=', partner_id),
+                                                           ('id', '!=', self._origin.id),
+                                                           ('is_primary', '=', True)])
+                for rec in contacts:
+                    rec.is_primary = False
         else:
             if self.primary_contact_count == 0:
                 raise ValidationError('Необходимо указать хотя бы один главный контакт')
